75530ddd19cac96dea22f66a0c65cf59f35ec8af
Bilbo-T-Baggins1 <38269337+Bilbo-T-Baggins1@users.noreply.github.com>
2018-04-10 23:36:50 -0500

    Add files via upload

---
+++ web code end of 4-10-2018 colton's portion.py
@@ -0,0 +1,50 @@
+# first input, question to the user
+
+import webbrowser
+
+# loop
+var = 1
+while var == 1:
+    # first input, question to the user
+web = input ('Where you want to go ')
+
+    # options for the user
+    if web == ('reddit'):
+        webbrowser.open ('https://www.reddit.com/')
+        print ('enjoy the front page of the internet! p.s. I recommend 50-50 or watchpeopledie')
+
+    elif web == ('youtube'):
+        webbrowser.open ('https://www.youtube.com/')
+        print ('Big Money Salvia Over and Out!')
+
+    elif web == ('pornhub'):
+        webbrowser.open ('https://www.pornhub.com/')
+        print ('Cya in 5 minutes you cuck')
+
+    elif web == ('facebook'):
+        print ('are you fucking crazy? I don"t want papa Zucc to spy in on us')
+
+    elif web == ('myspace'):
+        webbrowser.open ('https://myspace.com/')
+        print ('Cringe lvl = 100')
+
+    elif web == ('hackerrank'):
+        webbrowser.open ('https://www.hackerrank.com/')
+        print ('Gotta level up those scripts')
+
+    elif web == ('screenshare')
+        webbrowser.open ('https://www.screenleap.com/')
+        print ('get to coding')
+
+    elif web == ('alpha orbital')
+        webbrowser.open ('https://www.alpha-orbital.com/')
+        print ('boosting through the mail slot in 3...2...1...')
+
+    elif web == ('inara')
+        webbrowser.open ('https://inara.cz/')
+        print ('boosting through the mail slot in 3...2...1...')
+
+
+    # error message for the user
+    else:
+        print ('nah nigga')
